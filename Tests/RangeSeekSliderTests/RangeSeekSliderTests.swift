5442f76f8756e062a6bcde872c6bc77e6c87a512
Mert Ahmet Güneş <mert.gunes@tretton37.com>
2018-06-17 22:51:49 +0200

    Updated codebase to Swift 4

--- Tests/RangeSeekSliderTests/RangeSeekSliderTests.swift
+++ Tests/RangeSeekSliderTests/RangeSeekSliderTests.swift
@@ -3,11 +3,8 @@ import XCTest
 
 class RangeSeekSliderTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(RangeSeekSlider().text, "Hello, World!")
+		
     }
-
 
     static var allTests : [(String, (RangeSeekSliderTests) -> () throws -> Void)] {
         return [
